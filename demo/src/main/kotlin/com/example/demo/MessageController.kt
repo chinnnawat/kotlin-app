4657c864f8351a205ba6864eac67d6408bdb2acf
chinnawat <chinnawat.w@opsta.co.th>
2025-09-20 13:45:39 +0700

    first commit

---
+++ demo/src/main/kotlin/com/example/demo/MessageController.kt
@@ -0,0 +1,16 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class MessageController {
+    @GetMapping
+    fun listMessages() = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
+}
